8c6ce8ca736c0f300f75b2c938b1844a6cb2d9ed
svensgaard <hypermads@gmail.com>
2016-03-01 11:39:18 +0100

    Added a class that can access the snowball stemmer library and stem a string.

--- AnalyzerTFIDF/src/Stemming/WordStemmer.java
+++ AnalyzerTFIDF/src/Stemming/WordStemmer.java
@@ -5,10 +5,41 @@
  */
 package Stemming;
 
+import Stemming.Snowball.ext.danishStemmer;
+import Stemming.Snowball.ext.englishStemmer;
+
 /**
  *
  * @author Mads
  */
 public class WordStemmer {
+    danishStemmer danishStemmer; 
+    englishStemmer englishStemmer;
+    String langugage;
+
+    public WordStemmer() {
+       danishStemmer = new danishStemmer();
+       englishStemmer = new englishStemmer();
+       
+        //Set current
+        //Stem()
+        //Get Current
+    }
     
+    public void setLangugage(String langugage) {
+        this.langugage = langugage;
+    }
+    public String stem(String value) {
+        if(langugage.equals("danish")) {
+            danishStemmer.setCurrent(value);
+            danishStemmer.stem();
+            return danishStemmer.getCurrent();
+        } else if(langugage.equals("english")) {
+            englishStemmer.setCurrent(value);
+            englishStemmer.stem();
+            return englishStemmer.getCurrent();
+        } else {
+            return value;
+        }
+    }
 }
