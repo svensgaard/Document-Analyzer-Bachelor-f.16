17304a6545e02d61e1a89ab13e4fc59b9c847196
Bryan Clemensen <bryaanc@gmail.com>
2016-03-09 10:34:51 +0100

    TFIDF implemenation Cleanup

--- AnalyzerTFIDF/src/analyzertfidf/TextProcessor.java
+++ AnalyzerTFIDF/src/analyzertfidf/TextProcessor.java
@@ -26,39 +26,18 @@ import java.util.List;
  */
 public class TextProcessor {
 
-    ArrayList<String> mostCommonWords;
     WordStemmer stemmer = new WordStemmer();
 
     public TextProcessor() {
-        readCommonWords();
-    }
-
-    private void readCommonWords() {
-        mostCommonWords = new ArrayList<>();
         stemmer = new WordStemmer();
         stemmer.setLangugage("english");
-
-        try {
-            FileReader fr = new FileReader(new File("100MostUsedWords.txt"));
-            BufferedReader br = new BufferedReader(fr);
-            String line = "";
-
-            while ((line = br.readLine()) != null) {
-                mostCommonWords.add(line.toLowerCase());
-            }
-
-        } catch (FileNotFoundException ex) {
-            throw new Error(ex.getMessage());
-        } catch (IOException ex) {
-            throw new Error(ex.getMessage());
-        }
     }
-
-    public LinkedHashMap readFile(String fileName) {
+    
+    public HashMap<String, Integer> readFile(String fileName) {
 
         // Read file
         try {
-            BufferedReader reader = new BufferedReader(new FileReader(fileName+".txt"));
+            BufferedReader reader = new BufferedReader(new FileReader(fileName + ".txt"));
 
             HashMap<String, Integer> tempMap = new HashMap<>();
             String line = "";
@@ -72,64 +51,6 @@ public class TextProcessor {
                 for (String word : wordArray) {
                     // Remove letters
                     if (word.length() > 1) {
-                        // Remove most common words
-                        if (!mostCommonWords.contains(word)) {
-
-                            // Stem word
-                            stem = stemmer.stem(word);
-
-                            // Increment if word is known
-                            if (tempMap.containsKey(stem)) {
-                                tempMap.put(stem, tempMap.get(stem) + 1);
-                            } else {
-                                // Add new word and frequency to map
-                                tempMap.put(stem, 1);
-                            }
-                        }
-                    }
-                }
-            }
-            
-            LinkedHashMap sorted = sortHashMapByValuesD(tempMap);
-            
-//            writeToFile(fileName, sorted);
-            
-
-            
-            // Sort HashMap and return a LinkedHashMap
-            return sorted;
-
-        } catch (IOException e) {
-            throw new Error("Reading File Exception", e);
-        }
-    }
-    
-    public void writeToFile(String fileName, HashMap<String, Integer> keywords) throws IOException {
-        BufferedWriter bw = new BufferedWriter(new FileWriter(new File(fileName+"keywords.txt")));
-        
-        for (HashMap.Entry<String, Integer> entry : keywords.entrySet()) {
-            bw.write(entry.getKey() + ", " + entry.getValue()+"\n");
-        }
-        bw.close();
-        
-    }
-
-    public LinkedHashMap findKeywords(BufferedReader br) throws IOException {
-
-        HashMap<String, Integer> tempMap = new HashMap<>();
-        String line = "";
-        String stem = "";
-
-        // Split symbols
-        while ((line = br.readLine()) != null) {
-            String[] wordArray = line.toLowerCase().split("[\\d\\p{Punct}\\s]+");
-
-            // Generate HashMap with keys and values
-            for (String word : wordArray) {
-                // Remove letters
-                if (word.length() > 1) {
-                    // Remove most common words
-                    if (!mostCommonWords.contains(word)) {
 
                         // Stem word
                         stem = stemmer.stem(word);
@@ -144,10 +65,24 @@ public class TextProcessor {
                     }
                 }
             }
-        }
+            
+//            writeToFile(fileName, sorted);
 
-        // Sort HashMap and return a LinkedHashMap
-        return sortHashMapByValuesD(tempMap);
+            return tempMap;
+
+        } catch (IOException e) {
+            throw new Error("Reading File Exception", e);
+        }
+    }
+
+    public void writeToFile(String fileName, HashMap<String, Integer> keywords) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new FileWriter(new File(fileName + "keywords.txt")));
+
+        for (HashMap.Entry<String, Integer> entry : keywords.entrySet()) {
+            bw.write(entry.getKey() + ", " + entry.getValue() + "\n");
+        }
+        bw.close();
+
     }
 
     /**
@@ -184,7 +119,7 @@ public class TextProcessor {
         }
         return sortedMap;
     }
-    
+
     public LinkedHashMap sortHashMapByValuesDouble(HashMap passedMap) {
         List mapKeys = new ArrayList(passedMap.keySet());
         List mapValues = new ArrayList(passedMap.values());
